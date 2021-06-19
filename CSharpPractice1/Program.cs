cbfc1f645a004b8c0a5f6beadd5bea56f936795c
Nikita Zhiga <assisken@gmail.com>
2021-06-19 15:42:02 +0300

    Done third task
    Signed-off-by: Nikita Zhiga <assisken@gmail.com>

--- CSharpPractice1/Program.cs
+++ CSharpPractice1/Program.cs
@@ -7,21 +7,22 @@ namespace CSharpPractice1
         public static void Main()
         {
             Console.WriteLine("Выберите задание:");
+            Console.WriteLine("3. Написать программу, которая подсчитывает расстояние между точками с координатами");
             Console.WriteLine("4. Написать программу обмена значениями двух переменных типа int без использования вспомогательных методов.");
             Console.WriteLine("5. Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.");
-            Console.WriteLine("6. Создать класс с методами, которые могут пригодиться в вашей учебе ");
             Console.WriteLine("Любое другое значение -- Выход");
 
             var select = SixthProgram.ReadInt();
             switch (select)
             {
+                case 3:
+                    ThirdProgram.Run();
+                    break;
                 case 4:
                     FourthProgram.Run();
                     break;
                 case 5:
                     FifthProgram.Run();
-                    break;
-                case 6:
                     break;
             }
         }
---
+++ CSharpPractice1/ThirdProgram.cs
@@ -0,0 +1,36 @@
+﻿// 3.
+//
+// а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2, y2
+// по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2).
+// Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+// б) *Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.
+// Жига Никита
+
+using System;
+
+namespace CSharpPractice1
+{
+    public class ThirdProgram
+    {
+        public static void Run()
+        {
+            Console.Write("x1 = ");
+            var x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y1 = ");
+            var y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("x2 = ");
+            var x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y2 = ");
+            var y2 = Convert.ToInt32(Console.ReadLine());
+
+            var result = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
+            Console.WriteLine("Результат: {0:0.00}", result);
+            Console.WriteLine("Используя метод: {0:0.00}", MeasureDistance(x1, y1, x2, y2));
+        }
+        
+        static double MeasureDistance(int x1, int y1, int x2, int y2)
+        {
+            return Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2);
+        }
+    }
+}
