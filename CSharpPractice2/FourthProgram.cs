2b474c8f67e3c636313cf64eb803358127cfbc0a
Nikita Zhiga <assisken@gmail.com>
2021-06-20 20:11:43 +0300

    Done Fourth task
    Signed-off-by: Nikita Zhiga <assisken@gmail.com>

---
+++ CSharpPractice2/FourthProgram.cs
@@ -0,0 +1,39 @@
+﻿// Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль.
+// На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains).
+// Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль,
+// программа пропускает его дальше или не пропускает.
+// С помощью цикла do while ограничить ввод пароля тремя попытками.
+// Жига Никита
+
+using System;
+
+namespace CSharpPractice2
+{
+    public static class FourthProgram
+    {
+        public static void Run()
+        {
+            var attempts = 3;
+
+            while (attempts > 0)
+            {
+                Console.Write("Ваш логин: ");
+                var login = Console.ReadLine();
+                Console.Write("Ваш пароль: ");
+                var password = Console.ReadLine();
+
+                if (Check(login, password))
+                {
+                    Console.WriteLine("Аутентификация успешно пройдена. До свидания!");
+                    return;
+                }
+
+                attempts--;
+                Console.WriteLine($"Неверный логин или пароль. Попробуйте ещё раз. Осталось попыток: {attempts}");
+            }
+            Console.WriteLine("Все попытки исчерпаны, до свидания!");
+        }
+
+        static bool Check(string login, string password) => login == "root" && password == "GeekBrains";
+    }
+}
