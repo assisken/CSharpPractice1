303a8c4348abf3992ff22293af181158134c24b3
Nikita Zhiga <assisken@gmail.com>
2021-06-26 19:24:48 +0300

    Added boilerplate for run third practice
    Signed-off-by: Nikita Zhiga <assisken@gmail.com>

---
+++ CSharpPractice3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpPractice3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите задание:");
+            Console.WriteLine("1. Вычисления комплексных чисел");
+            Console.WriteLine("3. Демонстрация класса дробей");
+            Console.WriteLine("Любое другое значение -- Выход");
+
+            var select = CSharpPractice1.SixthProgram.ReadInt();
+            switch (select)
+            {
+                case 1:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
+}
