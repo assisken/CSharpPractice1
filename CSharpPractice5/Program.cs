00ef00ff9c79677dd23d1e0ad6d8fac808058876
Nikita Zhiga <assisken@gmail.com>
2021-07-05 00:09:31 +0300

    Done with third task
    Signed-off-by: Nikita Zhiga <assisken@gmail.com>

---
+++ CSharpPractice5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpPractice5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите задание:");
+            Console.WriteLine("3. Перестановка строк");
+            Console.WriteLine("4. Задача ЕГЭ");
+            Console.WriteLine("Любое другое значение -- Выход");
+
+            var select = CSharpPractice1.SixthProgram.ReadInt();
+            switch (select)
+            {
+                case 3:
+                    ThirdProgram.Run();
+                    break;
+            }
+
+        }
+    }
+}
